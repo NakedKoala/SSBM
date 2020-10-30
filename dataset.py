56368fb5256188a113f1601d2e31528519eb2d99
William Wen <william.wen@uwaterloo.ca>
2020-10-30 17:47:21 -0400

    add alignment

--- dataset.py
+++ dataset.py
@@ -7,45 +7,52 @@ import pdb
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 import traceback
-from common_parsing_logic import proc_df
+from common_parsing_logic import proc_df, align
 
 
 
 class SSBMDataset(Dataset):
 
-   
+
 
     val_ratio = 0.2
     button_press_indicator_dim = 12
     frame_delay = 2
-    def __init__(self, src_dir, char_id, opponent_id, device, ds_type=None):
+    def __init__(self, src_dir, char_id, opponent_id, device, window_size=0, ds_type=None):
         torch.manual_seed(0)
         self.csv_files = [ os.path.join(src_dir, fname) for fname in os.listdir(src_dir) if '.csv' in fname]
         self.features = []
         self.cts_targets = []
         self.bin_cls_targets = []
-        
+        self.window_size = window_size
+
         for csv_path in tqdm(self.csv_files):
             try:
                 df = pd.read_csv(csv_path, index_col="frame_index")
                 features, cts_targets, bin_cls_targets = proc_df(df, char_id, opponent_id, SSBMDataset.frame_delay, SSBMDataset.button_press_indicator_dim)
-              
-                self.features.append(features)
+
+                # align features
+                if window_size > 0:
+                    aligned_features_list = []
+                    align_queue = []
+                    for example in features:
+                        aligned_features_list.append(align(align_queue, window_size, example))
+                    aligned_features = torch.stack(aligned_features_list)
+                else:
+                    # window_size == 0 means don't use windows at all - for ANNs for example
+                    aligned_features = features
+
+                self.features.append(aligned_features)
                 self.cts_targets.append(cts_targets)
                 self.bin_cls_targets.append(bin_cls_targets)
             except:
                 print(f'failed to load {csv_path}')
                 traceback.print_exc()
-        
+
         self.features = torch.cat(self.features, dim=0)
         self.cts_targets = torch.cat(self.cts_targets, dim=0)
         self.bin_cls_targets = torch.cat(self.bin_cls_targets, dim=0)
 
-        shuffled_idx = torch.randperm(len(self.features))
-        self.features = self.features[shuffled_idx]
-        self.cts_targets = self.cts_targets[shuffled_idx]
-        self.bin_cls_targets = self.bin_cls_targets[shuffled_idx]
-        
         self.features = self.features.to(device)
         self.cts_targets =  self.cts_targets.to(device)
         self.bin_cls_targets =  self.bin_cls_targets.to(device)
@@ -57,8 +64,8 @@ class SSBMDataset(Dataset):
 
     def __len__(self):
         return len(self.features)
-       
+
 
     def __getitem__(self, idx):
         return self.features[idx], self.cts_targets[idx], self.bin_cls_targets[idx]
-    
+
--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from mvp_model import SSBM_MVP
 from train import train
 import torch
 from slippi import Game
-from infra_adaptor import convert_frame_to_input_tensor, convert_output_tensor_to_command
+from infra_adaptor import convert_frame_to_input_tensor, convert_output_tensor_to_command, FrameContext
 from common_parsing_logic import proc_button_press
 from mvp_model import SSBM_MVP
 
@@ -13,20 +13,22 @@ from mvp_model import SSBM_MVP
 # Sample usage for the parser
 # if __name__ == '__main__':
     # parser = SLPParser(src_dir="./dev_data_slp", dest_dir="./dev_data_csv")
+    # parser()
 
 
-# Sample usage: training 
+# Sample usage: training
 
 # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# # SSBMDataset has a window_size argument for RNNs
 # trn_ds = SSBMDataset(src_dir="./", char_id=2, opponent_id=1, device=device)
 # trn_dl = DataLoader(trn_ds, batch_size=256, shuffle=True, num_workers=0)
 # model = SSBM_MVP(100)
 # train(model, trn_dl, trn_dl, 100,  5000, device, [1] * 7)
 
 
-
 # Sample usage: infra adaptors
 
+"""
 model = SSBM_MVP(100)
 model.load_state_dict(torch.load('./weights/mvp_fit1_EP8_VL0078.pth',  map_location=lambda storage, loc: storage))
 model.eval()
@@ -36,6 +38,18 @@ frame = slp_object.frames[100]
 feature_tensor = convert_frame_to_input_tensor(frame, char_id=2, opponent_id=1)
 cts_targets, bin_cls_targets = model(feature_tensor)
 print(convert_output_tensor_to_command(cts_targets, bin_cls_targets))
+"""
 
-
-   
+# aligned version
+slp_object = Game("./(YOTB) Fox vs Falcon (MN) [FD] Game_20200222T152806.slp")
+frames = slp_object.frames[0:20]
+frame_ctx = FrameContext(window_size=10)
+# imitate streaming
+features_list = []
+for frame in frames:
+    features_list.append(frame_ctx.push_frame(frame, char_id=2, opponent_id=1))
+    # cts_targets, bin_cls_targets = model(features_list[-1])
+    # print(convert_output_tensor_to_command(cts_targets, bin_cls_targets))
+# play around with features_list...
+# e.g. print size
+print(features_list[0].shape)
