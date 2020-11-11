Fix LSTM + move scaling to common logic

lstm_model.py
@@ -44,8 +44,9 @@ class SSBM_LSTM(nn.Module):
          seq_len = x.shape[1]
 
          embed_indices, regular_feat = x[:,:,0:SSBM_LSTM.num_embedding_features].long(), x[:,:,SSBM_LSTM.num_embedding_features:]
-         action_embed_idx = embed_indices[:,:,:4]
-         button_combination_idx = embed_indices[:,:,4:]
+         action_embed_idx = torch.cat([embed_indices[:,:,0:2], embed_indices[:,:,3:5]] ,dim=1)
+    
+         button_combination_idx = torch.cat([embed_indices[:,:,2].reshape(batch_size, seq_len,1),embed_indices[:,:,5].reshape(batch_size, seq_len, 1)], dim=1)
         #  import pdb 
         #  pdb.set_trace()
          action_embed_feat = self.action_state_embedding(action_embed_idx.reshape(-1)).reshape(batch_size, seq_len, -1)

run.py
@@ -23,11 +23,11 @@ from tqdm import tqdm
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # # SSBMDataset has a window_size argument for RNNs
-trn_ds = SSBMDataset(src_dir="./", char_id=2, opponent_id=1, window_size=1, device=device)
-trn_dl = DataLoader(trn_ds, batch_size=256, shuffle=True, num_workers=0)
+trn_ds = SSBMDataset(src_dir="./", char_id=2, opponent_id=1, window_size=10, device=device)
+trn_dl = DataLoader(trn_ds, batch_size=1, shuffle=True, num_workers=0)
 
-# # model = SSBM_LSTM(100, 50, hidden_size=256, num_layers=1, bidirectional=False)
-model = SSBM_MVP(100, 50)
+model = SSBM_LSTM(100, 50, hidden_size=4, num_layers=1, bidirectional=False)
+# model = SSBM_MVP(100, 50)
 # # for batch in trn_dl:
 # #     feat, cts_targets, button_targets = batch
 # #     cts_o, logits_o = model(feat)